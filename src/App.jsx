8563a52acf42dc38c7930953d1b434305ab00294
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-09-18 14:19:00 -0300

    Adicionando video de fundo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import './App.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
-
+  AOS.init();
+  
   return (
     <>
-     
+
+    <div className='background'>
+      <video loop autoPlay muted>
+        <source src='../public/assets/background-video.mp4' type='video/mp4'/>
+      </video>
+    </div>
+
+
     </>
+
+    
   )
+  
 }
 
 export default App
---
+++ src/routes/index.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+
+const index = () => {
+  return (
+    <>
+        <BrowserRouter>
+            <Routes>
+                <Route path=""/>
+            </Routes>
+        </BrowserRouter> 
+    </>
+  )
+}
+
+export default index
